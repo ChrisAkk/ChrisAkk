da905a6fcba830b3f7a34f85c6009ed50e06d9dc
Chris MacBook <macbook@chris-macbook-1.home>
2026-05-20 17:21:33 +0200

    mise en place du site

---
+++ js/index.js
@@ -0,0 +1,229 @@
+// music
+
+const btnMusic = document.querySelector('.btn-music');
+const ambianceAudio = new Audio('/sons/deepblue.mp3')
+ambianceAudio.loop = true;
+ambianceAudio.volume = 0.1;
+
+btnMusic.addEventListener('click', () => {
+    if (btnMusic.innerHTML === '<i class="ri-volume-mute-line"></i>') {
+        ambianceAudio.play();
+        btnMusic.innerHTML = '<i class="ri-volume-up-line"></i>';
+    } else {
+        ambianceAudio.pause();
+        btnMusic.innerHTML = '<i class="ri-volume-mute-line"></i>'
+    }
+})
+
+// bouton tourne carte
+
+const turnBtn = document.querySelector('.turn-btn');
+const backBtn = document.querySelector('.back-btn');
+const card = document.querySelector('.global-card');
+
+const waveMusic = new Audio('/sons/wave.mp3')
+waveMusic.volume = 0.5;
+
+let data;
+let isAnimating = false;
+let isFlipped = false;
+let isClicked = false;
+
+if(turnBtn) {
+    turnBtn.addEventListener('click', () => {
+        card.style.transform = '';
+        card.classList.remove('unflipped');
+        card.classList.add('flipped');
+        waveMusic.currentTime = 0;
+        waveMusic.play();
+
+        isAnimating = true;
+        setTimeout(() => {
+            isFlipped = true;
+            card.classList.remove('flipped');
+            card.style.transform = 'rotateY(180deg)';
+            isAnimating = false;
+        }, 1000)
+    })
+}
+
+if (backBtn) {
+    backBtn.addEventListener('click', () => {
+        card.style.transform = '';
+        card.classList.remove('flipped');
+        card.classList.add('unflipped');
+        waveMusic.currentTime = 0;
+        waveMusic.play();
+
+        isAnimating = true;
+        setTimeout(() => {
+            isFlipped = false;
+            card.classList.remove('unflipped');
+            card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+            isAnimating = false;
+        }, 1000)
+    })
+}
+
+window.addEventListener('mousemove', (e) => {
+    if (isAnimating || !isClicked || !data) {
+        return;
+    }
+
+    if (!data) {
+        card.style.transition = 'none';
+        data = card.getBoundingClientRect();
+    }
+
+    let ecartX = (e.clientX - data.left) - (data.width / 2);
+    let ecartY = (e.clientY - data.top) - (data.height / 2);
+
+    let rotateX = -ecartY / 20;
+    let rotateY = ecartX / 20;
+
+    if (isFlipped) {
+        card.style.transform = `rotateX(${rotateX}deg) rotateY(${180 + rotateY}deg)`
+    } else {
+        card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+    }
+})
+
+card.addEventListener('mousedown', () => {
+    if (isAnimating){
+        return;
+    } 
+
+    isClicked = true;
+    card.style.transition = 'none';
+    data = card.getBoundingClientRect();
+})
+
+window.addEventListener('mouseup', () => {
+    isClicked = false; 
+
+    card.style.transition = 'transform 0.3s cubic-bezier(0.25, 1, 0.5, 1)';
+
+    if (isFlipped) {
+        card.style.transform = `rotateX(0deg) rotateY(180deg)`;
+    } else {
+        card.style.transform = `rotateX(0deg) rotateY(0deg)`;
+    }
+
+    data = null;
+})
+
+// bulles 
+
+const aquarium = document.querySelector('.aquarium');
+
+window.addEventListener('pageshow', (e) => {
+    if (aquarium) {
+        aquarium.innerHTML = '';
+    }
+})
+
+const tailles = ["1px", "2px", "3px", "4px", "5px"]
+let compteur = 0;
+
+const generateur = setInterval(() => {
+    if (compteur >= 50) {
+        clearInterval(generateur);
+        return;
+    }
+
+    const bulle = document.createElement('div');
+    bulle.classList.add('bulle')
+    bulle.style.left = `${Math.random() * 100}%`
+    let indice = Math.floor(Math.random() * tailles.length)
+    bulle.style.width = tailles[indice]
+    bulle.style.height = tailles[indice]
+    aquarium.appendChild(bulle)
+    compteur++;
+
+}, 300)
+
+// Changement de langue 
+
+const translation = {
+    fr : {
+        title : "Étudiant full-stack | Mathématiques & Informatique",
+        btnCv : "Télécharger le CV",
+        btnContact : 'Me contacter <i class="ri-arrow-turn-forward-line"></i>',
+        reseaux : "Me joindre",
+        contact : "Mes réseaux",
+        portfolio : '<i class="ri-user-fill"></i> Mon portfolio complet arrive très bientôt.',
+        btnBack : 'Retour <i class="ri-arrow-turn-forward-line"></i>',
+        txtEnd : "Carte Virtuelle de Chris | Portfolio à venir"
+    },
+
+    en : {
+        title : "Full-stack Student | Mathematics & Computer Science",
+        btnCv : "Download CV",
+        btnContact : 'Contact me <i class="ri-arrow-turn-forward-line"></i>',
+        reseaux : "Socials",
+        contact : "Contact",
+        portfolio : ' <i class="ri-user-fill"></i> Full portfolio coming very soon.',
+        btnBack : 'Flip back <i class="ri-arrow-turn-forward-line"></i>',
+        txtEnd : "Chris' Virtual Card | Portfolio coming soon"
+    }
+}
+
+const btnLanguage = document.querySelector('.btn-language');
+const languagContainer = document.querySelector('.language-container');
+
+const txtAbout = document.querySelector('.about');
+const btnCv = document.querySelector('.btn-cv');
+const btnContact = document.querySelector('.turn-btn');
+const txtReseau = document.querySelector('.title-reseaux');
+const txtContact = document.querySelector('.title-contact');
+const txtPortfolio = document.querySelector('.txt-portfolio');
+const btnBack = document.querySelector('.back-btn');
+const txtEnd = document.querySelector('.txt-end');
+
+btnLanguage.addEventListener('click', () => {
+    if (btnLanguage.dataset.lang === "en") {
+        btnLanguage.dataset.lang = "fr";
+
+        txtAbout.innerHTML = translation['fr']['title'];
+        btnCv.innerHTML = translation['fr']['btnCv'];
+        btnContact.innerHTML = translation['fr']['btnContact'];
+        txtReseau.innerHTML = translation['fr']['reseaux'];
+        txtContact.innerHTML = translation['fr']['contact'];
+        txtPortfolio.innerHTML = translation['fr']['portfolio'];
+        btnBack.innerHTML = translation['fr']['btnBack'];
+        txtEnd.innerHTML = translation['fr']['txtEnd'];
+
+        const notification = document.createElement('div');
+        notification.innerHTML = "Français";
+        notification.classList.add('notification');
+
+        languagContainer.appendChild(notification);
+
+        notification.addEventListener('animationend', () => {
+            notification.remove();
+        })
+
+    } else {
+        btnLanguage.dataset.lang = "en";
+
+        txtAbout.innerHTML = translation['en']['title'];
+        btnCv.innerHTML = translation['en']['btnCv'];
+        btnContact.innerHTML = translation['en']['btnContact'];
+        txtReseau.innerHTML = translation['en']['reseaux'];
+        txtContact.innerHTML = translation['en']['contact'];
+        txtPortfolio.innerHTML = translation['en']['portfolio'];
+        btnBack.innerHTML = translation['en']['btnBack'];
+        txtEnd.innerHTML = translation['en']['txtEnd'];
+
+        const notification = document.createElement('div');
+        notification.innerHTML = "English";
+        notification.classList.add('notification');
+
+        languagContainer.appendChild(notification);
+
+        notification.addEventListener('animationend', () => {
+            notification.remove();
+        })
+
+    }
+})
